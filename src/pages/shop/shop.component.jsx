2f74e86757a209c9142c21983cf3280a9c5565dc
FernandoNv <fernandonvieira47@hotmail.com>
2021-01-25 11:42:39 -0300

    updating shop and directory components into functional components and edited so they can use the data from redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
-import SHOP_DATA from './shop.data.js';
 
-class ShopPage extends React.Component{
-    constructor(props){
-        super(props);
-        
-        this.state = {
-            collections: SHOP_DATA
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectShopCollections } from '../../redux/shop/shop.selectors';
+
+const ShopPage  = ({ collections }) =>(
+    <div className="shop-page">
+        {
+            collections.map(({id, ...otherCollectionProps}) => (
+                <CollectionPreview key={id} {...otherCollectionProps}  />
+            ))
         }
-    }
+    </div>
+)
 
-    render(){
-        const { collections } = this.state;
-        return(
-            <div className="shop-page">
-                {
-                    collections.map(({id, ...otherCollectionProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionProps}  />
-                    ))
-                }
-            </div>
-        )
-    }
-}
+const mapStateToProps = createStructuredSelector({
+    collections: selectShopCollections
+});
 
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
